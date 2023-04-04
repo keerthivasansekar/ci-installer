77922d28806254244d7f1fce947148838f558f54
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-04-04 15:55:14 +0530

    Prerequisites done

--- app/Views/installation/install.php
+++ app/Views/installation/install.php
@@ -43,62 +43,34 @@
                         </div>
                     </fieldset>
 
-                    <h3>Connect Bank Account</h3>
+                    <h3>Prerequisites</h3>
                     <fieldset>
-                        <h2>Connect Bank Account</h2>
-                        <p class="desc">Please enter your infomation and proceed to next step so we can build your account</p>
+                        <h2>Check prerequisite</h2>
+                        <p class="desc">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
                         <div class="fieldset-content">
-                            <div class="form-group">
-                                <label for="find_bank" class="form-label">Find Your Bank</label>
-                                <div class="form-find">
-                                    <input type="text" name="find_bank" id="find_bank" placeholder="Ex. Techcombank" />
-                                    <input type="submit" value="Search" class="submit">
-                                    <span class="form-icon"><i class="zmdi zmdi-search"></i></span>
-                                </div>
-                            </div>
-                            <div class="choose-bank">
-                                <p class="choose-bank-desc">Or choose from these popular bank</p>
-                                <div class="form-radio-flex">
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_1" value="bank_1" checked="checked" />
-                                        <label for="bank_1"><img src="./assets/images/bank-1.jpg" alt=""></label>
-                                    </div>
-
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_2" value="bank_2" />
-                                        <label for="bank_2"><img src="./assets/images/bank-2.jpg" alt=""></label>
-                                    </div>
-
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_3" value="bank_3" />
-                                        <label for="bank_3"><img src="./assets/images/bank-3.jpg" alt=""></label>
-                                    </div>
-
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_4" value="bank_4" />
-                                        <label for="bank_4"><img src="./assets/images/bank-4.jpg" alt=""></label>
-                                    </div>
-
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_5" value="bank_5" />
-                                        <label for="bank_5"><img src="./assets/images/bank-5.jpg" alt=""></label>
-                                    </div>
-
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_6" value="bank_6" />
-                                        <label for="bank_6"><img src="./assets/images/bank-6.jpg" alt=""></label>
-                                    </div>
-                                    
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_7" value="bank_7" />
-                                        <label for="bank_7"><img src="./assets/images/bank-7.jpg" alt=""></label>
-                                    </div>
-
-                                    <div class="form-radio-item">
-                                        <input type="radio" name="choose_bank" id="bank_8" value="bank_8" />
-                                        <label for="bank_8"><img src="./assets/images/bank-8.jpg" alt=""></label>
-                                    </div>
-                                </div>
+                            <div class="prerequisite-container">
+                                <ul class="prerequisite-list">
+                                    <li class="prerequisite-check">
+                                        <img class="prerequisite-check-img" src="./assets/images/check-mark.png" alt="">
+                                        PHP version > 7.4 or 8.0
+                                    </li>
+                                    <li>
+                                        <img class="prerequisite-check-img" src="./assets/images/check-mark.png" alt="">    
+                                        Intl extension enabled
+                                    </li>
+                                    <li>
+                                        <img class="prerequisite-check-img" src="./assets/images/cross-icon.png" alt="">
+                                        Mbstring extension enabled
+                                    </li>
+                                    <li>
+                                        <img class="prerequisite-check-img" src="./assets/images/check-mark.png" alt="">
+                                        Writable folder is writable
+                                    </li>
+                                    <li>
+                                    <img class="prerequisite-check-img" src="./assets/images/check-mark.png" alt="">
+                                        Public folder is accessible
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </fieldset>
